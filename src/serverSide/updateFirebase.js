65d5194df328e83d77ed85595fd2a12d0b551df5
Blaz Bagic <blaz.bagic@gmail.com>
2019-02-28 14:06:24 +0100

    Improved server scripts, added an error logger

--- src/serverSide/updateFirebase.js
+++ src/serverSide/updateFirebase.js
@@ -6,6 +6,7 @@ const cinestar = require('./scrapers/cinestar.js');
 const imdb = require('./scrapers/imdb.js');
 const progress = require('./util/progressBar.js');
 const firebase = require('./storage/firebase.js');
+const { logger } = require('./util/logger.js');
 
 
 /*************************************************************/
@@ -14,29 +15,49 @@ const firebase = require('./storage/firebase.js');
 updateFirebase()
     .then(() => process.exit(0))
     .catch(err => {
-        console.log(err);
+        console.log('\nCritical error occured, check the logs.');
+        logger.log({
+            level: 'critical',
+            message: err,
+            traceback: err.stack,
+        });
+        logger.error('Critical: ' + err);
         process.exit(1);
     });
-
 
 /*************************************************************/
 /************************** Functions ************************/
 /*************************************************************/
 
-/**  */
+/** Enters new movie&schedule data in the database. */
 async function updateFirebase() {
+    logger.info("Started firebase update job.")
+
     let links = await cinestar.getLinksOfAllMovies();
+    logger.info("Success fetching all cinestar links.")
+
     let current = 0;
     let total = links.length;
 
     console.log("Scraping and saving movie data to firebase:");
-    // for (link of links) {
-    //     progress.printProgressBar(current, total);
-    //     current++;
+    for (link of links) {
+        progress.printProgressBar(current, total);
+        current++;
 
-    //     let movieItem = await cinestar.scrapeMovieInfo(link);
-    //     movieItem = await imdb.addIMDBValuesToMovie(movieItem);
-    //     await firebase.addMovieToFirebase(movieItem);
-    // }
+        let movieItem = await cinestar.scrapeMovieInfo(link);
+
+        let movieExists = await firebase.movieExists(movieItem.title);
+        if (!movieExists) {
+            movieItem = await imdb.addIMDBValuesToMovie(movieItem);
+            await firebase.addMovieToFirebase(movieItem);
+        }
+        await firebase.updateSchedule(movieItem);
+    }
     progress.printProgressBar(total, total);
+    logger.info("Successfully updated the database.")
+}
+
+async function updateFirebaseSchedule(link) {
+    let movieItem = await cinestar.scrapeMovieInfo(link);
+    await firebase.updateSchedule(movieItem);
 }
